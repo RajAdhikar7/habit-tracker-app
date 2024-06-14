0fe68f8d965fcaa0e884b5e3b9bd100fd49778f4
RajAdhikar7 <adhikarraj77@gmail.com>
2024-06-14 19:08:04 +0530

    all changes made

--- habittracker/habits/views.py
+++ habittracker/habits/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import render ,redirect  ,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Habit,HabitEvent
 from .forms import HabitForm,HabitCompletionForm
+from django.forms.models import modelformset_factory
 
 @login_required
 def list_view(request):
-    habit_objects = Habit.objects.filter(user=request.user)
-    #habit_objects = get_object_or_404(Habit,user=request.user)
+    habit_objects = Habit.objects.filter(user=request.user).prefetch_related('habitevent_set')
     context = {
-        "object_list":habit_objects
+        "object_list": habit_objects,
     }
-    return render(request, "habits/list-view.html", context=context) 
+    return render(request, "habits/list-view.html", context=context)
 
 @login_required
 def add_habit(request):
@@ -25,6 +25,38 @@ def add_habit(request):
         habit.save()
         return redirect("habit_list")
     return render(request,"habits/add-habits.html",context)
+
+@login_required
+def habit_update_view(request, id=None):
+    obj = get_object_or_404(Habit, pk=id, user=request.user)
+    form = HabitForm(request.POST or None, instance=obj)
+    HabitEventFormset = modelformset_factory(HabitEvent, form=HabitCompletionForm, extra=1)  # Allow 1 extra form for new HabitEvent
+    qs = obj.habitevent_set.all()
+    formset = HabitEventFormset(request.POST or None, queryset=qs)
+    
+    if all([form.is_valid(), formset.is_valid()]):
+        parent = form.save(commit=False)
+        parent.save()
+        for form in formset:
+            child = form.save(commit=False)
+            if form.cleaned_data:  # Ensure the form has data before saving
+                child.habit = parent
+                child.save()
+        context = {
+            "form": form,
+            "formset": formset,
+            "object": obj,
+            "message": "Data saved."
+        }
+    else:
+        context = {
+            "form": form,
+            "formset": formset,
+            "object": obj
+        }
+
+    return render(request, "habits/create-update.html", context)
+
 
 
 @login_required
@@ -51,7 +83,5 @@ def habit_detail(request , id):
     }
     return render(request,"habits/detail.html",context=context)
 
-from django.contrib.auth.models import User
-from .models import HabitEvent
-from datetime import date
+
 
